eb1509c4b4a9cecca93a4cb10100f40935bd9843
sibanand-cis <sibananda.k@cisinlabs.com>
2015-03-16 17:49:51 +0530

    add result code

--- lib/ghostinspector_ruby/result.rb
+++ lib/ghostinspector_ruby/result.rb
@@ -9,5 +9,10 @@ module GhostinspectorRuby
       @data = attributes["data"]
     end
 
+    def self.find(result_id)
+      response = Faraday.get("#{API_URL}/results/#{result_id}/?apiKey=#{API_KEY}")
+      result = JSON.parse(response.body)
+      new(result)
+    end
   end
 end
--- test/results/result_test.rb
+++ test/results/result_test.rb
@@ -5,4 +5,12 @@ class GhostInspectorResultTest < Minitest::Test
   def test_exists_results
     assert GhostinspectorRuby::Result
   end
+
+  def test_gives_back_a_single_result
+    VCR.use_cassette('get_result') do
+      result = GhostinspectorRuby::Result.find('5502f1e706454d3c06f33380')
+
+      assert_equal 'SUCCESS', result.code
+    end
+  end
 end
