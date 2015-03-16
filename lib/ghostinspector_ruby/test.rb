05dca10b1dea8452d41447c6928443fbb0d2f6fe
sibanand-cis <sibananda.k@cisinlabs.com>
2015-03-16 17:50:47 +0530

    add code for tests

--- lib/ghostinspector_ruby/test.rb
+++ lib/ghostinspector_ruby/test.rb
@@ -3,7 +3,38 @@ module GhostinspectorRuby
   require 'json'
   require 'byebug'
 
+
+
   class Test
 
+    attr_reader :code, :data
+    def initialize(attributes)
+      @code = attributes["code"]
+      @data = attributes["data"]
+    end
+
+    def self.all
+      response = Faraday.get("#{API_URL}/tests/?apiKey=#{API_KEY}")
+      tests = JSON.parse(response.body)
+      new(tests)
+    end
+
+    def self.find(test_id)
+      response = Faraday.get("#{API_URL}/tests/#{test_id}/?apiKey=#{API_KEY}")
+      test = JSON.parse(response.body)
+      new(test)
+    end
+
+    def self.execute(test_id)
+      response = Faraday.get("#{API_URL}/tests/#{test_id}/execute/?apiKey=#{API_KEY}")
+      result = JSON.parse(response.body)
+      new(result)
+    end
+
+    def self.get_results(test_id)
+      response = Faraday.get("#{API_URL}/tests/#{test_id}/results/?apiKey=#{API_KEY}")
+      result = JSON.parse(response.body)
+      new(result)
+    end
   end
 end
--- test/tests/tests_test.rb
+++ test/tests/tests_test.rb
@@ -8,7 +8,38 @@ class GhostInspectorTestsTest < Minitest::Test
 
   def test_gives_back_all_tests
     VCR.use_cassette('all_tests') do
+      tests = GhostinspectorRuby::Test.all
 
+      assert_equal 1, tests.data.length
+      assert tests.data.kind_of?(Array)
+      assert tests.kind_of?(GhostinspectorRuby::Test)
+    end
+  end
+
+  def test_gives_back_a_single_test
+    VCR.use_cassette('get_test') do
+      test = GhostinspectorRuby::Test.find('54aa1af73f9fcf332dda7b06')
+
+      assert_equal 'SUCCESS', test.code
+      assert_equal 'Click Consent',test.data['name']
+    end
+  end
+
+  def test_execute_the_test
+    VCR.use_cassette('execute_test') do
+      execute_test = GhostinspectorRuby::Test.execute('54aa1af73f9fcf332dda7b06')
+      assert_equal GhostinspectorRuby::Test, execute_test.class
+
+      assert_equal 'SUCCESS', execute_test.code
+      assert_equal true, execute_test.data['passing']
+    end
+  end
+
+  def test_list_of_results_of_a_test
+    VCR.use_cassette('list_of_results_of_a_test') do
+      result_list = GhostinspectorRuby::Test.get_results('54aa1af73f9fcf332dda7b06')
+
+      assert_equal 'SUCCESS', result_list.code
     end
   end
 end
