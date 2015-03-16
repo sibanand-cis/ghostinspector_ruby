f572aff8dbbd3b5dfb10604424461ac54ddfe557
sibanand-cis <sibananda.k@cisinlabs.com>
2015-03-16 17:51:52 +0530

    include api_key and api_url in GhostinspectorRuby modulr

--- lib/ghostinspector_ruby.rb
+++ lib/ghostinspector_ruby.rb
@@ -5,4 +5,6 @@ require_relative "ghostinspector_ruby/result"
 
 module GhostinspectorRuby
   # Your code goes here...
+  API_URL = 'https://api.ghostinspector.com/v1'
+  API_KEY = 'YOUR API KEY'
 end
